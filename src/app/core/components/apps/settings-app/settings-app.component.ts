e55326fdeb25e7761b87825f5fce7ecf987df11f
Lucas Bernard <lucas-fabien.bernard@regiocom.com>
2025-02-25 19:02:59 +0100

    added background setting

--- src/app/core/components/apps/settings-app/settings-app.component.ts
+++ src/app/core/components/apps/settings-app/settings-app.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {NgForOf, TitleCasePipe} from '@angular/common';
+import {ConfigStore} from '../../../store/config.store';
+import {TippyDirective} from '@ngneat/helipopper';
 
 @Component({
   selector: 'app-settings-app',
-  imports: [],
+  imports: [
+    NgForOf,
+    TippyDirective,
+    TitleCasePipe
+  ],
   templateUrl: './settings-app.component.html',
   styleUrl: './settings-app.component.scss'
 })
 export class SettingsAppComponent {
+  private readonly configStore = inject(ConfigStore)
 
+  backgrounds = [
+    "cat.jpg",
+    "landscape.jpg",
+    "minimalistic.jpg"
+  ]
+
+  changeBackground(background: string) {
+    this.configStore.set("background", background)
+  }
 }
